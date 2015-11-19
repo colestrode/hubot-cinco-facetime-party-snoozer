ac3a80406c34f082a0453b1be537d3ae0288c7ee
Cole Furfaro-Strode <cfurfarostrode@coles-mbp.home>
2015-11-19 07:01:19 -0500

    fixed linting and bugs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var snoozer = require('./lib/snoozer')
+var _ = require('lodash')
   , volume = require('./lib/volume')
   , rooms = require('./lib/rooms')
   , responses = require('./lib/responses')
@@ -7,7 +7,8 @@ var snoozer = require('./lib/snoozer')
 module.exports = function(robot) {
   robot.brain.on('loaded', function() {
     robot.brain.snoozer = robot.brain.snoozer || {};
-    snoozer.init(robot.brain.snoozer);
+    volume.init(robot.brain.snoozer);
+    rooms.init(robot.brain.snoozer);
   });
 
   robot.respond(/cinco help/, helptext);
@@ -15,7 +16,7 @@ module.exports = function(robot) {
   robot.respond(/cinco (facetime )?(party )?snoozer/i, function(msg) {
     var room = msg.message.room;
 
-    if(rooms.has(room)) {
+    if (rooms.has(room)) {
       rooms.remove(room);
       msg.send('Cinco Facetime Party Snoozer is powered off! You\'ll have to make your own conversation now');
     } else {
@@ -26,18 +27,36 @@ module.exports = function(robot) {
 
   robot.respond(/cinco status/i, function(msg) {
     var room = msg.message.room;
-    if(rooms.has(room)) {
+    if (rooms.has(room)) {
       msg.send('Cinco Facetime Party Snoozer is on and ready to talk!');
     } else {
       msg.send('Cinco Facetime Party Snoozer is off, maybe you should turn it on so this place feels lively!');
     }
   });
 
-  robot.respond(/cinco louder/i, volume.louder.bind(volume));
-  robot.respond(/cinco loudest/i, volume.loudest.bind(volume));
-  robot.respond(/cinco quieter/i, volume.quieter.bind(volume));
-  robot.respond(/cinco quietest/i, volume.quietest.bind(volume));
+  robot.respond(/cinco louder/i, function(msg) {
+    var room = msg.message.room;
+    volume.louder(room);
+    msg.respond('Volume set to ' + volume.value(room));
+  });
 
+  robot.respond(/cinco loudest/i, function(msg) {
+    var room = msg.message.room;
+    volume.loudest(room);
+    msg.respond('Volume set to ' + volume.value(room));
+  });
+
+  robot.respond(/cinco quieter/i, function(msg) {
+    var room = msg.message.room;
+    volume.quieter(room);
+    msg.respond('Volume set to ' + volume.value(room));
+  });
+
+  robot.respond(/cinco quietest/i, function(msg) {
+    var room = msg.message.room;
+    volume.quieter(room);
+    msg.respond('Volume set to ' + volume.value(room));
+  });
 
   // the magic
   robot.hear(/.*/i, function(msg) {
