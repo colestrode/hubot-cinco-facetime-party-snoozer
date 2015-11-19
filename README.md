85b15af983c929cd1d345c990470b24134ce9222
Cole Furfaro-Strode <cfurfarostrode@coles-mbp.home>
2015-11-19 06:49:45 -0500

    refactoring, added volume commands

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
-# hubot-cinco-facetime-party-snoozer
+# Hubot-cinco-facetime-party-snoozer
 Thanks for coming!
 
-Tired of talking and having no one respond to you? Let Hubot be your friend! Now with hubot-cinco-facetime-party-snoozer 
+Tired of talking and having no one respond to you? Let Hubot be your friend! Now with Hubot-cinco-facetime-party-snoozer 
 Hubot will converse with you like a normal human with 5 professional responses!
 
-Turn it on for a room by typing `hubot cinco facetime party snoozer`. Turn it off by typing the same thing! It's that simple!
-
 ## What?
-Hubot Cinco Facetime Party Snoozer is channel specific. Choose your channels wisely.
+Hubot Cinco Facetime Party Snoozer will let Hubot participate in the conversation in your channel just like a real boy. 
+
+Turn it on for a channel by typing `Hubot cinco facetime party snoozer`. Turn it off by typing the same thing! It's that simple!
+
+Choose your channels wisely.
 
 ## Why?
 This seemed like a good idea at the time.
 
 https://www.youtube.com/watch?v=kg2UlYDswSc
+
+## Commands
+
+* `cinco facetime party snoozer`: Toggles Hubot's Cinco Facetime Party Snoozer for the current channel.
+* `cinco status`: Whether Cinco Facetime Party Snoozer is on in this channel.
+* `cinco louder`: Makes Hubot respond more often.
+* `cinco quieter`: Make Hubot respond less often.
+* `cinco loudest`: Makes Hubot respond all the time.
+* `cinco quietest`: Makes Hubot respond not very often at all.
+* `cinco help`: Prints out these commands for you
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var snoozer = require('./lib/snoozer');
+var snoozer = require('./lib/snoozer')
+  , volume = require('./lib/volume')
+  , rooms = require('./lib/rooms')
+  , responses = require('./lib/responses')
+  , helptext = require('./lib/helptext');
 
 module.exports = function(robot) {
   robot.brain.on('loaded', function() {
@@ -6,22 +10,45 @@ module.exports = function(robot) {
     snoozer.init(robot.brain.snoozer);
   });
 
-  robot.respond(/cinco (facetime)? (party)? snoozer/i, function(msg) {
+  robot.respond(/cinco help/, helptext);
+
+  robot.respond(/cinco (facetime )?(party )?snoozer/i, function(msg) {
     var room = msg.message.room;
 
-    msg.send(snoozer.toggleRoom(room));
+    if(rooms.has(room)) {
+      rooms.remove(room);
+      msg.send('Cinco Facetime Party Snoozer is powered off! You\'ll have to make your own conversation now');
+    } else {
+      rooms.add(room);
+      msg.send('Cinco Facetime Party Snoozer is on! Now get to talking about whatever boring thing you want!');
+    }
   });
 
-  robot.hear(/.*/i, function(msg) {
-    var room = msg.message.room
-      , response = snoozer.respondMaybe(room);
+  robot.respond(/cinco status/i, function(msg) {
+    var room = msg.message.room;
+    if(rooms.has(room)) {
+      msg.send('Cinco Facetime Party Snoozer is on and ready to talk!');
+    } else {
+      msg.send('Cinco Facetime Party Snoozer is off, maybe you should turn it on so this place feels lively!');
+    }
+  });
 
-    if (response) {
-      msg.send(response);
+  robot.respond(/cinco louder/i, volume.louder.bind(volume));
+  robot.respond(/cinco loudest/i, volume.loudest.bind(volume));
+  robot.respond(/cinco quieter/i, volume.quieter.bind(volume));
+  robot.respond(/cinco quietest/i, volume.quietest.bind(volume));
+
+
+  // the magic
+  robot.hear(/.*/i, function(msg) {
+    var room = msg.message.room;
+
+    if (rooms.has(room) && _.random(volume.min, volume.max) <= volume.value()) {
+      msg.send(responses.random());
     }
   });
 
   robot.respond(/cinco debug/i, function(msg) {
-    msg.send(snoozer.debug());
+    msg.send(JSON.stringify(robot.brain.snooze, null, 2));
   });
 };
